cec82f55720836b5a044ef77fa3f82172f90b380
sunilthitme <thitamesunil808@gmail.com>
2021-12-14 15:52:46 +0530

    Employee is present part time

--- EmployeWage.java
+++ EmployeWage.java
@@ -5,13 +5,19 @@ public class EmployeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage Computation");
 		int hour=8;
+		int partHour=4;
 		int perHour=20;
 		int dailyHour=0;
 		
-		double ran=Math.random();
-		if (ran>0.5) {
+		double ran=(int)(Math.floor(Math.random()*10))%3+1;
+		if (ran==1) {
 			System.out.println("Employee is Present");
 			 dailyHour=hour*perHour;
+			System.out.println("Daily Employee Wage is:: "+dailyHour);
+		}
+		else if(ran==2) {
+			System.out.println("Employee is Present part time");
+			 dailyHour=partHour*perHour;
 			System.out.println("Daily Employee Wage is:: "+dailyHour);
 		}
 		else {
