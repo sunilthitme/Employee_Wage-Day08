2acd75164aac969001683a52c9803d46c8a8b559
sunilthitme <thitamesunil808@gmail.com>
2021-12-14 15:33:08 +0530

    Employee Presenty

---
+++ EmployeWage.java
@@ -0,0 +1,17 @@
+package com.employeeDay08;
+
+public class EmployeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage Computation");
+		
+		double ran=Math.random();
+		if (ran>0) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+
+}
