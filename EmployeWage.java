e1359ddebe5807e42afa91de306686483722672f
sunilthitme <thitamesunil808@gmail.com>
2021-12-14 15:49:32 +0530

    Employee Daily Wage

--- EmployeWage.java
+++ EmployeWage.java
@@ -4,13 +4,19 @@ public class EmployeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage Computation");
+		int hour=8;
+		int perHour=20;
+		int dailyHour=0;
 		
 		double ran=Math.random();
-		if (ran>0) {
+		if (ran>0.5) {
 			System.out.println("Employee is Present");
+			 dailyHour=hour*perHour;
+			System.out.println("Daily Employee Wage is:: "+dailyHour);
 		}
 		else {
 			System.out.println("Employee is Absent");
+			System.out.println("Daily Employee Wage is:: "+dailyHour);
 		}
 	}
 
